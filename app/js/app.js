f9cb8caecb49959efad8cd6c026df323c1823013
Leon3854 <frad109@mail.ru>
2021-08-03 20:40:27 +0500

    Finish of work on the meneger slider

--- app/js/app.js
+++ app/js/app.js
@@ -5,8 +5,8 @@
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
-import {Swiper, Parallax, Mousewheel, Controller} from 'swiper'
-Swiper.use([Parallax, Mousewheel, Controller])
+import {Swiper, Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation} from 'swiper'
+Swiper.use([Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation])
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -23,7 +23,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		speed: 2400,
 		mousewheel: {
     	invert: false, // это нужно для того что бы при скроле колёсика мышки в низ у нас менялся слайд 
-  	}
+  	},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: true
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
 	})
 
 	swiperImg.controller.control = swipreText
